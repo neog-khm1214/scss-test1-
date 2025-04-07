8d719ad10a6ec4929eb6af9a5d63291b86bb3cee
neog-khm1214 <test_mm@neograph.co.kr>
2025-04-07 18:25:21 +0900

    페이지전환_수정진행중
    처음 오픈시 간헐적으로 나타남.
    새로고침시에도 처음 오픈시와 같이 나와야함.

--- 페이지전환/script.js
+++ 페이지전환/script.js
@@ -1,15 +1,27 @@
 let pannel = document.querySelector(".pannel");
 let content = document.querySelector(".main");
 let nextBtn = document.getElementById("nextPage");
-let count = "";
-setTimeout(() => {
-  content.classList.add("fade-in");
-  pannel.classList.add("fade-in");
-}, 500);
-setTimeout(() => {
-  pannel.classList.remove("fade-in");
+let count = 290;
+if (self.window.name.indexOf("justAwordToNowisYourWindow") != 0) {
+  //처음 페이지 접근시
+  self.window.name = "justAwordToNowisYourWindow";
+  //alert("first Load");
   pannel.classList.add("reset");
-}, 1500);
+  content.classList.add("fade-in");
+} else {
+  //새로고침이나 페이지 전환시
+  //새로고침할때마
+  //alert("reload");
+
+  setTimeout(() => {
+    content.classList.add("fade-in");
+    pannel.classList.add("fade-in");
+  }, count);
+  setTimeout(() => {
+    pannel.classList.remove("fade-in");
+    pannel.classList.add("reset");
+  }, count * 3);
+}
 
 nextBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -17,5 +29,5 @@ nextBtn.addEventListener("click", function (e) {
   pannel.classList.add("fade-out");
   setTimeout(() => {
     window.location.href = this.href;
-  }, 1500); // CSS transition 시간과 맞추기
+  }, count * 2); // CSS transition 시간과 맞추기
 });
